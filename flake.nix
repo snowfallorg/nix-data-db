0a1de07eae3aa1ecdcf83556555dd0c0f44a8082
Victor Fuentes <vmfuentes64@gmail.com>
2022-11-27 15:25:54 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    nix-data-generator.url = "github:snowflakelinux/nix-data-generator";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, nix-data-generator, flake-compat }:
+    utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          packages = [
+            brotli
+            nix-data-generator.defaultPackage.${system}
+            sqlite
+          ];
+        };
+      });
+}
